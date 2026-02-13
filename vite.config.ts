81c97413afd7879068ae20bcb18b046034b5b4ed
Flopper <floppertvon@gmail.com>
2026-02-13 21:21:30 +0200

    Update base path in Vite configuration
    Set base path for deployment to '/FlopperSite/'.

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+  // fix for that shitty mistake i did
+  base: '/FlopperSite/',
+
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
